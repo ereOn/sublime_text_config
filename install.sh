8e574837c4da7f8508d820209b36f2d099a13df2
Julien Kauffmann <julien.kauffmann@freelan.org>
2014-03-03 17:56:46 +0100

    Handle Package Control

--- install.sh
+++ install.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 
-SETTINGS_PATH="$HOME/Library/Application Support/Sublime Text 3/Packages"
-USER_SETTINGS_PATH="$SETTINGS_PATH/User"
+ROOT_PATH="$HOME/Library/Application Support/Sublime Text 3"
+PACKAGES_PATH="$ROOT_PATH/Packages"
+INSTALL_PACKAGES_PATH="$ROOT_PATH/Installed Packages"
+USER_SETTINGS_PATH="$PACKAGES_PATH/User"
 LOCAL_USER_SETTING_PATH="$(pwd)/User"
 SUBL_PATH="/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl"
 SUBL_INSTALL_PATH="/usr/bin/subl"
+PACKAGE_CONTROL_URL="https://sublime.wbond.net/Package%20Control.sublime-package"
 
 # Install the subl command line tool.
 sudo ln -f -s "${SUBL_PATH}" "${SUBL_INSTALL_PATH}"
 
 # Install the settings
-mkdir -p "${SETTINGS_PATH}"
+mkdir -p "${PACKAGES_PATH}"
+mkdir -p "${INSTALL_PACKAGES_PATH}"
 ln -f -s "${LOCAL_USER_SETTING_PATH}" "${USER_SETTINGS_PATH}"
+
+if [ ! -f "${INSTALL_PACKAGES_PATH}/Package Control.sublime-package" ]; then
+	cd "${INSTALL_PACKAGES_PATH}" && wget ${PACKAGE_CONTROL_URL}
+fi
